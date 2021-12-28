4131f7ed9b12a9593d5e91510af619259def2066
nc619 <nc619@ic.ac.uk>
2021-12-28 17:41:21 +0000

    Why is it not working

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -3,7 +3,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
 
-@WebServlet(urlPatterns={"/patients"},loadOnStartup = 1)
+@WebServlet(urlPatterns={"/patients","/"},loadOnStartup = 1)
 public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
